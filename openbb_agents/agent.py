777bf7a4187a95c911b6b2e6b37dbf47018b61d6
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2024-04-06 19:04:19 -0400

    allow to provide OpenBB PAT when running agent

--- openbb_agents/agent.py
+++ openbb_agents/agent.py
@@ -14,79 +14,85 @@ from openbb_agents.tools import (
     map_openbb_routes_to_langchain_tools,
 )
 from openbb_agents.utils import get_dependencies
+from openbb import obb
 
 from . import VERBOSE
 
 logger = logging.getLogger(__name__)
 
 
-def openbb_agent(
-    query: str, openbb_tools: Optional[list[str]] = None, verbose=VERBOSE
-) -> str:
-    """Answer a query using the OpenBB Agent equipped with tools.
+class OpenBBAgent:
+    def __init__(self, personal_access_token: str, verbose=VERBOSE):
+        # Login to OpenBB using person access token
+        obb.account.login(pat=personal_access_token)
+        self.verbose = verbose
 
-    By default all available openbb tools are used. You can have a query
-    answered using a smaller subset of OpenBB tools by using the `openbb_tools`
-    argument.
+    def answer_query(self, query: str, openbb_tools: Optional[list[str]] = None) -> str:
+        """Answer a query using the OpenBB Agent equipped with tools and a personal access token.
 
-    Parameters
-    ----------
-    query : str
-        The query you want to have answered.
-    openbb_tools : optional[list[str]]
-        Optional. Specify the OpenBB collections or commands that you use to use. If not
-        specified, every available OpenBB tool will be used.
+        By default all available openbb tools are used. You can have a query
+        answered using a smaller subset of OpenBB tools by using the `openbb_tools`
+        argument.
 
-    Examples
-    --------
-    >>> # Use all OpenBB tools to answer the query
-    >>> openbb_agent("What is the market cap of TSLA?")
-    >>> # Use only the specified tools to answer the query
-    >>> openbb_agent("What is the market cap of TSLA?",
-    ...              openbb_tools=["/equity/fundamental", "/equity/price/historical"])
+        Parameters
+        ----------
+        query : str
+            The query you want to have answered.
+        openbb_tools : optional[list[str]]
+            Optional. Specify the OpenBB collections or commands that you want to use. If not
+            specified, every available OpenBB tool will be used.
 
-    """
+        Examples
+        --------
+        >>> agent = OpenBBAgent(personal_access_token="your_token_here")
+        >>> # Use all OpenBB tools to answer the query
+        >>> agent.answer_query("What is the market cap of TSLA?")
+        >>> # Use only the specified tools to answer the query
+        >>> agent.answer_query("What is the market cap of TSLA?",
+        ...                    openbb_tools=["/equity/fundamental", "/equity/price/historical"])
 
-    subquestion_list = generate_subquestions(query, verbose=verbose)
-    logger.info("Generated subquestions: %s", subquestion_list)
+        """
 
-    if openbb_tools:
-        tools = map_openbb_routes_to_langchain_tools(openbb_tools)
-    else:
-        tools = get_all_openbb_tools()
-    vector_index = create_tool_index(tools=tools)
+        subquestion_list = generate_subquestions(query, verbose=self.verbose)
+        logger.info("Generated subquestions: %s", subquestion_list)
 
-    answered_subquestions = []
-    for subquestion in subquestion_list.subquestions:  # TODO: Do in parallel
-        # Fetch tool for subquestion
-        logger.info("Attempting to select tools for: %s", {subquestion.question})
-        selected_tools = select_tools(
-            vector_index=vector_index,
-            tools=tools,
-            subquestion=subquestion,
-            answered_subquestions=answered_subquestions,
-            verbose=verbose,
-        )
-        # TODO: Improve filtering of tools (probably by storing them in a dict)
-        tool_names = [tool.name for tool in selected_tools.tools]
-        subquestion_tools = [tool for tool in tools if tool.name in tool_names]
-        logger.info("Retrieved tool(s): %s", tool_names)
+        if openbb_tools:
+            tools = map_openbb_routes_to_langchain_tools(openbb_tools)
+        else:
+            tools = get_all_openbb_tools()
+        vector_index = create_tool_index(tools=tools)
 
-        # Then attempt to answer subquestion
-        answered_subquestion = generate_subquestion_answer(
-            SubQuestionAgentConfig(
-                query=query,
+        answered_subquestions = []
+        for subquestion in subquestion_list.subquestions:  # TODO: Do in parallel
+            # Fetch tool for subquestion
+            logger.info("Attempting to select tools for: %s", {subquestion.question})
+            selected_tools = select_tools(
+                vector_index=vector_index,
+                tools=tools,
                 subquestion=subquestion,
-                tools=subquestion_tools,
-                dependencies=get_dependencies(
-                    answered_subquestions, subquestion
-                ),  # TODO: Just do this in gneerate_subquestion_answer
-            ),
-            verbose=verbose,
-        )
-        answered_subquestions.append(answered_subquestion)
+                answered_subquestions=answered_subquestions,
+                verbose=self.verbose,
+            )
+            # TODO: Improve filtering of tools (probably by storing them in a dict)
+            tool_names = [tool.name for tool in selected_tools.tools]
+            subquestion_tools = [tool for tool in tools if tool.name in tool_names]
+            logger.info("Retrieved tool(s): %s", tool_names)
 
-    # Answer final question
-    return generate_final_response(
-        query=query, answered_subquestions=answered_subquestions, verbose=verbose
-    )
+            # Then attempt to answer subquestion
+            answered_subquestion = generate_subquestion_answer(
+                SubQuestionAgentConfig(
+                    query=query,
+                    subquestion=subquestion,
+                    tools=subquestion_tools,
+                    dependencies=get_dependencies(
+                        answered_subquestions, subquestion
+                    ),  # TODO: Just do this in generate_subquestion_answer
+                ),
+                verbose=self.verbose,
+            )
+            answered_subquestions.append(answered_subquestion)
+
+        # Answer final question
+        return generate_final_response(
+            query=query, answered_subquestions=answered_subquestions, verbose=self.verbose
+        )
--- run.py
+++ run.py
@@ -1,20 +1,29 @@
 import argparse
 
-from openbb_agents import agent
+from openbb_agents.agent import OpenBBAgent
 
+# Initialize the argument parser
 parser = argparse.ArgumentParser(description="Query the OpenBB agent.")
+# Add argument for the query
 parser.add_argument(
     "query", metavar="query", type=str, help="The query to send to the agent."
 )
+# Add argument for the personal access token
+parser.add_argument(
+    "-t", "--token", type=str, required=True, help="Your personal access token for the OpenBB agent."
+)
+# Add argument for verbose logging
 parser.add_argument(
     "-v", "--verbose", action="store_true", help="Enable verbose logging."
 )
+# Parse the arguments
 args = parser.parse_args()
 
-# We only import after passing in command line args to have verbosity propagate.
+# Create an instance of OpenBBAgent with the provided token and verbosity setting
+agent = OpenBBAgent(personal_access_token=args.token, verbose=args.verbose)
 
-query = args.query
-result = agent.openbb_agent(query, verbose=args.verbose)
+# Use the agent to answer the query
+result = agent.answer_query(args.query)
 
 print("============")
 print("Final Answer")
